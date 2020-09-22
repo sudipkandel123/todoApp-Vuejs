f55086f1e847ab78238e0709a5523942bec3a875
sudip kandel <sudip.kandel@bridgei2i.com>
2020-09-22 11:56:08 +0530

    Final code added  TODO app

--- test/src/components/layout/header.vue
+++ test/src/components/layout/header.vue
@@ -1,25 +1,29 @@
 <template>
-<header class="header">
-    <h1>Todo List</h1>
-</header>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "header"
+  name: "Header"
 }
 </script>
 
 <style scoped>
-.header {
+  .header {
     background: #333;
     color: #fff;
     text-align: center;
     padding: 10px;
-}
-
-.header a {
+  }
+  .header a {
     color: #fff;
     padding-right: 5px;
-}
+    text-decoration: none;
+  }
 </style>
