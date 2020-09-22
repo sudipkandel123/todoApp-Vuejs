4404b5669bc6ff0dd123ac106a3fa9e1cbcb1688
sudip kandel <sudip.kandel@bridgei2i.com>
2020-09-22 10:41:04 +0530

    Working Todo App

---
+++ test/src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+        <todoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+</div>
+</template>
+
+<script>
+import todoItem from "./todoItem"
+export default {
+    name: "Todos",
+    components: {
+        todoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
